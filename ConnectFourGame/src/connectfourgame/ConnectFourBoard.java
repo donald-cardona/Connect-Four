b5777c489a54e371608fce2585e5b6bfad57e570
Donald Cardona <dfc901@gmail.com>
2018-08-11 13:28:50 -0400

    Implemented win by 4 in a row, column, diagonal, and anti-diagonal

--- ConnectFourGame/src/connectfourgame/ConnectFourBoard.java
+++ ConnectFourGame/src/connectfourgame/ConnectFourBoard.java
@@ -20,12 +20,22 @@ public class ConnectFourBoard extends BorderPane{
     private String turn;
     private Label winner;
     private int count;
+    private char[][] mark;
     
     public ConnectFourBoard() {
         grid = new GridPane();
         turn = "red";
         winner = new Label("");
         count = 0;
+        mark = new char[][] {
+            {'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+            {'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+            {'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+            {'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+            {'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+            {'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+                    
+        };
         
         for(int i = 0; i < 6; i++) {
             for(int j = 0; j < 7; j++) {
@@ -35,8 +45,10 @@ public class ConnectFourBoard extends BorderPane{
                     public void handle(MouseEvent event) {
                         if(turn.equalsIgnoreCase("red")){
                             connect.paintRed();
+                            mark[(int)grid.getRowIndex(connect)][(int)grid.getColumnIndex(connect)] = 'r';
                             turn = "yellow";
                             count++;
+                            checkWin();
                             
                             connect.setDisable(true);
                             if(grid.getChildren().indexOf(connect) > 6)
@@ -44,8 +56,10 @@ public class ConnectFourBoard extends BorderPane{
                         }
                         else if(turn.equalsIgnoreCase("yellow")){
                             connect.paintYellow();
+                            mark[(int)grid.getRowIndex(connect)][(int)grid.getColumnIndex(connect)] = 'y';
                             turn = "red";
                             count++;
+                            checkWin();
                             
                             connect.setDisable(true);
                             if(grid.getChildren().indexOf(connect) > 6)
@@ -62,5 +76,57 @@ public class ConnectFourBoard extends BorderPane{
         grid.setPrefSize(410, 350);
         this.getChildren().add(grid);
         
+    }
+    
+    public void checkWin() {
+        boolean isWin = false;
+        
+        //for loop to check win by 4 in a row
+        for(int i = 0; i < 6; i++) {
+            for(int j = 0; j < 4; j++) {
+                if(mark[i][0+j] == mark[i][1+j] && mark[i][1+j] == mark[i][2+j] && mark[i][2+j] == mark[i][3+j] && mark[i][1+j] == 'r') {
+                    System.out.println("Red wins");
+                }
+                else if(mark[i][0+j] == mark[i][1+j] && mark[i][1+j] == mark[i][2+j] && mark[i][2+j] == mark[i][3+j] && mark[i][1+j] == 'y') {
+                    System.out.println("Yellow wins");
+                }
+            }
+        }
+        
+        //for loop to check win by 4 in a column
+        for(int n = 0; n < 7; n++ ) {
+            for(int m = 0; m < 3; m++) {
+                if(mark[0+m][n] == mark[1+m][n] && mark[1+m][n] == mark[2+m][n] && mark[2+m][n] == mark[3+m][n] && mark[1+m][n] == 'r') {
+                    System.out.println("Red wins");
+                }
+                else if(mark[0+m][n] == mark[1+m][n] && mark[1+m][n] == mark[2+m][n] && mark[2+m][n] == mark[3+m][n] && mark[1+m][n] == 'y') {
+                    System.out.println("Yellow wins");
+                }
+            }
+        }
+        
+        //for loop to check win by 4 in a forward diagonal
+        for(int y = 0; y < 4; y++ ) {
+            for(int x = 0; x < 3; x++) {
+                if(mark[x+3][y] == mark[x+2][y+1] && mark[x+2][y+1] == mark[x+1][y+2] && mark[x+1][y+2] == mark[x][y+3] && mark[x+2][y+1] == 'r') {
+                    System.out.println("Red wins");
+                }
+                else if(mark[x+3][y] == mark[x+2][y+1] && mark[x+2][y+1] == mark[x+1][y+2] && mark[x+1][y+2] == mark[x][y+3] && mark[x+2][y+1] == 'y') {
+                    System.out.println("Yellow wins");
+                }
+            }
+        }
+        
+        //for loop to checl win by 4 in a backward diagonal
+        for(int y = 3; y > -1; y-- ) {
+            for(int x = 0; x < 3; x++) {
+                if(mark[x][y] == mark[x+1][y+1] && mark[x+1][y+1] == mark[x+2][y+2] && mark[x+2][y+2] == mark[x+3][y+3] && mark[x+1][y+1] == 'r') {
+                    System.out.println("Red wins");
+                }
+                else if(mark[x][y] == mark[x+1][y+1] && mark[x+1][y+1] == mark[x+2][y+2] && mark[x+2][y+2] == mark[x+3][y+3] && mark[x+1][y+1] == 'y') {
+                    System.out.println("Yellow wins");
+                }
+            }
+        }
     }
 }
--- ConnectFourGame/src/connectfourgame/ConnectFourGame.java
+++ ConnectFourGame/src/connectfourgame/ConnectFourGame.java
@@ -6,10 +6,7 @@
 package connectfourgame;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
